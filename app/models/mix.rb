274d2880bc66e9afd88505e5ae3dd49989ca81a3
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-09 17:26:43 +0300

    Added generated likes

--- app/models/mix.rb
+++ app/models/mix.rb
@@ -1,2 +1,21 @@
 class Mix < ApplicationRecord
+
+    before_create :random_likes, :random_plays, :random_downloads
+
+
+    def random_likes
+        self.likes = rand(5000...10000)
+    end
+
+    def random_plays
+        self.plays = rand(4000...5000)
+    end
+
+    def random_downloads
+        self.downloads = rand(1000...4000)
+    end
+
+
+
+
 end
